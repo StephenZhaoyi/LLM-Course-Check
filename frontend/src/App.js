43ed3f24e78fd8943fc9378ea06f0d0639810406
8arry <1173309602@qq.com>
2024-12-21 16:52:47 +0100

    loginpage-1(withoutapi)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,34 +8,34 @@ import Sidebar from "./components/Layout/Sidebar";
 
 // this is the temporary landing page for the app
 function App() {
-	return (
-		<Router>
-			<Navbar />
-			<Sidebar />
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<div>
-							<div className="text-center mt-24">
-								<h1 className="text-3xl font-semibold mb-6 text-tum-blue">
-									Ray, 我要 shamepoo
-								</h1>
-								<NavLink
-									to="/applicants"
-									className="bg-tum-blue text-white px-4 py-2 rounded hover:bg-blue-700 transition"
-								>
-									Go to Applicant List Page
-								</NavLink>
-							</div>
-						</div>
-					}
-				/>
-				<Route path="/applicants" element={<ApplicantListPage />} />
-				<Route path="/applicant/:id" element={<ApplicantDetailPage />} />
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <div className="text-center mt-24">
+                <h1 className="text-3xl font-semibold mb-6 text-tum-blue">
+                  Ray, 我要 shamepoo
+                </h1>
+                <NavLink
+                  to="/applicants"
+                  className="bg-tum-blue text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+                >
+                  Go to Applicant List Page
+                </NavLink>
+              </div>
+            </div>
+          }
+        />
+        <Route path="/applicants" element={<ApplicantListPage />} />
+        <Route path="/applicant/:id" element={<ApplicantDetailPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
