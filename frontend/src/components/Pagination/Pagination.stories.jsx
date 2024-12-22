3f58904d69cd78493d02300dc329290239d77eef
Tsaichen Lo <b09705059@gmail.com>
2024-12-22 21:58:10 +0100

    refactor: remove useless comment

--- frontend/src/components/Pagination/Pagination.stories.jsx
+++ frontend/src/components/Pagination/Pagination.stories.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import Pagination from "./Pagination";
 
-// Default export for Storybook
 export default {
   title: "Components/Pagination",
   component: Pagination,
   parameters: {
-    layout: "centered", // Centers the component in the Canvas
+    layout: "centered",
   },
   argTypes: {
     totalPages: {
@@ -20,12 +19,11 @@ export default {
       defaultValue: 1,
     },
     setCurrentPage: {
-      action: "setCurrentPage", // Logs the page change in the Storybook Actions panel
+      action: "setCurrentPage",
     },
   },
 };
 
-// Template to demonstrate usage of the component
 const Template = (args) => {
   const [currentPage, setCurrentPage] = useState(args.currentPage);
 
@@ -38,21 +36,18 @@ const Template = (args) => {
   );
 };
 
-// Default Story
 export const Default = Template.bind({});
 Default.args = {
   totalPages: 10,
   currentPage: 1,
 };
 
-// Story with More Pages
 export const ManyPages = Template.bind({});
 ManyPages.args = {
   totalPages: 20,
   currentPage: 1,
 };
 
-// Story with Fewer Pages
 export const FewPages = Template.bind({});
 FewPages.args = {
   totalPages: 5,
--- frontend/src/pages/ApplicantListPage.stories.jsx
+++ frontend/src/pages/ApplicantListPage.stories.jsx
@@ -1,13 +1,8 @@
 import ApplicantListPage from "./ApplicantListPage.stories";
 
-// Default export for Storybook
 export default {
 	title: "Pages/ApplicantListPage",
 	component: ApplicantListPage,
-	// tags: ["autodocs"],
-	// parameters: {
-	// 	layout: "fullscreen", // Displays the component in fullscreen mode
-	// },
 	argTypes: {},
 };
 
